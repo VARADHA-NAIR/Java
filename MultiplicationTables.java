3e91003236a9efddef9c2d10856ba4055ad6ed0d
VARADHA-NAIR <varadhanair97@gmail.com>
2025-08-26 13:40:50 +0530

    Multiplication Table

--- MultiplicationTables.java
+++ MultiplicationTables.java
@@ -7,9 +7,9 @@ public class MultiplicationTables
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number : ");
+        System.out.print("Enter a number : ");
         int num = sc.nextInt();
-        System.out.println("Enter the limit of numbers: ");
+        System.out.print("Enter the limit of numbers:");
         int limit = sc.nextInt();
         for(int i =1;i<=limit;i++)
         {
