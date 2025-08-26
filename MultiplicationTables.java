374b7689feb99200aff9d9c2cf5efa0dae105f88
VARADHA-NAIR <varadhanair97@gmail.com>
2025-08-26 13:36:57 +0530

    Multiplication Tables

---
+++ MultiplicationTables.java
@@ -0,0 +1,21 @@
+package Aug_26_2025.java;
+
+import java.util.Scanner;
+
+public class MultiplicationTables
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number : ");
+        int num = sc.nextInt();
+        System.out.println("Enter the limit of numbers: ");
+        int limit = sc.nextInt();
+        for(int i =1;i<=limit;i++)
+        {
+            int result = num*i;
+            System.out.println(num + "*" + i + " = "+result);
+        }
+
+    }
+}
